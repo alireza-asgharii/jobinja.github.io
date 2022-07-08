3d29f9a3eb1575baa90d4c76aee5864af6e1585e
Alireza <alirezwa03@gmail.com>
2022-07-08 16:02:39 +0430

    creat Tools container

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
+const userContainer = document.querySelector(".user-container");
+const moreTools = document.querySelector(".moreTools");
 const hamburgerMenu = document.querySelector(".hamburgerMenu");
 const hamburgerMenuContainer = document.querySelector(
   ".hamburgerMenuContainer"
 );
 
 let clickMenu = false;
+let clickTools = false;
 
 hamburgerMenu.addEventListener("click", hamburgerMenuHandler);
+userContainer.addEventListener("click", toolsHandler);
+
+
 
 function hamburgerMenuHandler() {
   clickMenu = !clickMenu;
@@ -16,4 +22,15 @@ function hamburgerMenuHandler() {
   }
   hamburgerMenuContainer.classList.remove("hamburgerMenuOpen");
   hamburgerMenuContainer.classList.add("hamburgerMenuClose");
+}
+
+function toolsHandler() {
+  clickTools = !clickTools;
+  if (clickTools) {
+    moreTools.classList.remove("toolsClose");
+    moreTools.classList.add("toolsShow");
+    return;
+  }
+  moreTools.classList.remove("toolsShow");
+  moreTools.classList.add("toolsClose");
 }
