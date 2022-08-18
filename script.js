cd5bbed8436eb4036ecdf1d63323872f4dbc3107
Alireza <alirezwa03@gmail.com>
2022-08-18 15:15:08 +0430

    create main section and Handler script

--- script.js
+++ script.js
@@ -82,9 +82,223 @@ for (let i = 0; i < cityItems.length; i++) {
   cityItems[i].addEventListener("click", () => {
     const value = cityItems[i].innerHTML;
     city.innerHTML = value;
-    slected(city.innerHTML, value)
+    slected(city.innerHTML, value);
   });
 }
 
+const todayData = [
+  { name: "استخدام کارشناس سئو", location: " سپاهان همراه ", city: "تهران" },
+  {
+    name: "استخدام کارشناس پشتیبانی رزرواسیون",
+    location: " سفرهای کیش",
+    city: "تهران",
+  },
+  {
+    name: "استخدام مدیر کسب و کار اینترنتی",
+    location: " سپاهان همراه ",
+    city: "تهران",
+  },
+  
+  {
+    name: 'استخدام انیماتور (خانم)',
+    location: ' تیزر مارکت ',
+    city: "تهران",
+  },
+  {
+    name: "استخدام کارشناس ارشد مالی",
+    location: " تجارت الکترونیک اول ",
+    city: "تهران",
+  },
+  {
+    name: "استخدام کارشناس بازرگانی خارجی",
+    location: " نیرو موتور سهند ",
+    city: "رشت",
+  },
+  
+  {
+    name: 'استخدام انیماتور (خانم)',
+    location: ' تیزر مارکت ',
+    city: "تهران",
+  },
+  
+  {
+    name: 'استخدام انیماتور (خانم)',
+    location: ' تیزر مارکت ',
+    city: "تهران",
+  },
+  
+  {
+    name: "استخدام Operation Senior Specialist",
+    location: " گروه اسنپ",
+    city: "تهران",
+  },
+  
+  {
+    name: 'استخدام انیماتور (خانم)',
+    location: ' تیزر مارکت ',
+    city: "تهران",
+  },
+  
 
+  {
+    name: "استخدام مدرس برنامه‌نویسی الگوریتم و پایتون",
+    location: " مجتمع فنی تهران نمایندگی نارمک",
+    city: "تهران",
+  },
+  {
+    name: "استخدام مدیر کسب و کار اینترنتی",
+    location: " سپاهان همراه ",
+    city: "تهران",
+  },
+  {
+    name: "استخدام مدیر کسب و کار اینترنتی",
+    location: " سپاهان همراه ",
+    city: "تهران",
+  },
+  {
+    name: "استخدام کارشناس پشتیبانی رزرواسیون",
+    location: " سفرهای کیش",
+    city: "تهران",
+  },
+  {
+    name: "استخدام مدرس برنامه‌نویسی الگوریتم و پایتون",
+    location: " مجتمع فنی تهران نمایندگی نارمک",
+    city: "تهران",
+  },
+  {
+    name: 'استخدام انیماتور (خانم)',
+    location: ' تیزر مارکت ',
+    city: "تهران",
+  },
+];
 
+const todayDataRender = () => {
+  const items = document.querySelector(".Landing .items");
+  for (let i of todayData) {
+    const markup = `
+      <a class="item" href="#">
+        <span class="fast">فوری</span>
+        <h6>${i.name}</h6>
+        <div class="more">
+          <div>
+            <i class="fa-solid fa-hotel"></i>
+            <span>${i.location}</span>
+          </div>
+          <div>
+            <i class="fa-solid fa-location-dot"></i>
+            <span>${i.city}</span>
+          </div>
+        </div>
+      </a>
+    `;
+    items.innerHTML += markup;
+  }
+};
+
+todayDataRender();
+
+//last Arr
+const lastData = [
+  {
+    name: 'استخدام مسئول دفتر(خانم)', 
+    location: ' باگلوس',
+    city: 'تهران'
+  },
+  {
+    name: 'استخدام طراح و گرافیست (خانم)', 
+    location: ' همراهان نور شبکه',
+    city: 'تهران'
+  },
+  {
+    name: 'استخدام سرپرست فروش', 
+    location: ' کیان تجارت فیدار',
+    city: 'تهران'
+  },
+  {
+    name: 'استخدام مسئول دفتر(خانم)', 
+    location: ' باگلوس',
+    city: 'تهران'
+  },
+  
+  
+  {
+    name: 'استخدام مسئول مناقصات', 
+    location: ' مهندسی اندیشه سبز تازه',
+    city: 'تهران'
+  },
+  {
+    name: 'استخدام کارآموز برنامه نویسی فلاتر', 
+    location: ' راه کار درست دیجیتال',
+    city: 'تهران'
+  },
+  {
+    name: 'استخدام مسئول دفتر(خانم)', 
+    location: ' تهران پارس',
+    city: 'تهران'
+  },
+  {
+    name: 'استخدام طراح و گرافیست (خانم)', 
+    location: ' همراهان نور شبکه',
+    city: 'تهران'
+  },
+  {
+    name: 'استخدام مسئول دفتر(خانم)', 
+    location: ' باگلوس',
+    city: 'تهران'
+  },
+  {
+    name: 'استخدام کارآموز برنامه نویسی فلاتر', 
+    location: ' راه کار درست دیجیتال',
+    city: 'تهران'
+  },
+  {
+    name: 'استخدام مسئول دفتر(خانم)', 
+    location: ' تهران پارس',
+    city: 'تهران'
+  },
+  
+  {
+    name: 'استخدام سرپرست فروش', 
+    location: ' کیان تجارت فیدار',
+    city: 'تهران'
+  },
+  {
+    name: 'استخدام مسئول مناقصات', 
+    location: ' مهندسی اندیشه سبز تازه',
+    city: 'تهران'
+  },
+  {
+    name: 'استخدام منشی آموزشگاه زبان(خانم)', 
+    location: ' عصر کلام زنگنه',
+    city: 'تهران'
+  },
+]
+
+const lastDataRender = () => {
+  const lastUl = document.querySelector('.Landing ul');
+  for (let item of lastData) {
+    const markup = `
+      <li class="item">
+        <a href="#">
+          <h6>
+          ${item.name}
+          </h6>
+          <div class="more">
+            <div>
+              <i class="fa-solid fa-hotel"></i>
+              <span> ${item.location}</span>
+            </div>
+            <div>
+              <i class="fa-solid fa-location-dot"></i>
+              <span>${item.city}</span>
+            </div>
+          </div>
+          <span class="in">امروز</span>
+        </a>
+      </li>
+    `
+    lastUl.innerHTML += markup;
+  }
+}
+
+lastDataRender()
